98a8f7c6269199843f545466893ce876cdfd2c49
Benjamin Montgomery <bmontgom@montynet.org>
2019-01-21 18:00:03 -0600

    fix getParks search and do calculations in meters

--- PotaMaps/Park Information/ParkData.swift
+++ PotaMaps/Park Information/ParkData.swift
@@ -11,6 +11,11 @@ import UIKit
 import CoreData
 import CoreLocation
 
+enum ParkDataErrors: Error {
+    case InitError
+    case DataFetchError(msg: String)
+}
+
 class ParkData {
 
     struct SearchBounds {
@@ -20,44 +25,54 @@ class ParkData {
 
     /// Get an array of parks within a specified range of the device's current
     /// location.
-    func getParks(withinRange range: Double, ofLocation currentLocation: CLLocation) -> [PotaParks]? {
+    ///
+    /// - parameter range: distance in meters
+    /// - parameter ofLocation: center point of a circular region to find parks within
+    func getParks(withinRange range: Double, ofLocation location: CLLocationCoordinate2D) throws -> [PotaParks] {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            throw ParkDataErrors.InitError
+        }
+        
+        // set initial search bounds
+        let searchBounds = calculateSearchDelta(forDistance: range, atLatitude: location.latitude)
+        var searchCriteria = [String: Double]()
+        searchCriteria["latUpper"] = location.latitude + (searchBounds.latDelta / 2.0)
+        searchCriteria["latLower"] = location.latitude - (searchBounds.latDelta / 2.0)
+        searchCriteria["longUpper"] = location.longitude + (searchBounds.longDelta / 2.0)
+        searchCriteria["longLower"] = location.longitude - (searchBounds.longDelta / 2.0)
+
         // get initial list of parks from CoreData
-        let searchBounds = calculateSearchDelta(forRange: range, atLatitude: currentLocation.coordinate.latitude)
         let fetchRequest: NSFetchRequest<PotaParks> = PotaParks.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "locationName", ascending: true)]
         fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-            NSPredicate(format: "latitude < %@", currentLocation.coordinate.latitude + searchBounds.latDelta),
-            NSPredicate(format: "latitude > %@", currentLocation.coordinate.latitude - searchBounds.latDelta),
-            NSPredicate(format: "longitude < %@", currentLocation.coordinate.longitude + searchBounds.longDelta),
-            NSPredicate(format: "longitude > %@", currentLocation.coordinate.longitude - searchBounds.longDelta)
+            NSPredicate(format: "latitude BETWEEN {%lf, %lf}", searchCriteria["latLower"]!, searchCriteria["latUpper"]!),
+            NSPredicate(format: "longitude BETWEEN {%lf, %lf}", searchCriteria["longLower"]!, searchCriteria["longUpper"]!),
         ])
 
         var initialResult: [PotaParks]
         do {
-            initialResult = try fetchRequest.execute()
+            initialResult = try appDelegate.persistentContainer.viewContext.fetch(fetchRequest)
         } catch {
-            return nil
+            throw ParkDataErrors.DataFetchError(msg: error.localizedDescription)
         }
 
         // refine list of parks for requested range
-        let searchRegion = CLCircularRegion(center: currentLocation.coordinate, radius: CLLocationDistance(range), identifier: "parkSearchRadius")
-
-        var finalParkList: [PotaParks] = []
-        for park in initialResult {
-            if searchRegion.contains(park.coordinate) {
-                finalParkList.append(park)
-            }
-        }
-
-        return finalParkList
+        let searchRegion = CLCircularRegion(center: location, radius: CLLocationDistance(range), identifier: "parkSearchRadius")
+        return initialResult.filter { searchRegion.contains($0.coordinate) }
     }
 
-    private func calculateSearchDelta(forRange range: Double, atLatitude latitude: Double) -> SearchBounds {
+    /**
+     Calculate a specified distance in degrees of latitude and longitude.
+     - parameter range: distance in meters
+     - parameter latitude: the latitude of the location where the delta is calculated
+     - returns: a `SearchBounds` struct with distances in degrees
+    */
+    private func calculateSearchDelta(forDistance distance: Double, atLatitude latitude: Double) -> SearchBounds {
         // get latitude delta
-        let latDelta = (range / 69)
+        let latDelta = (distance / 110574.0)
 
         // get longitude delta
-        let longDelta = range / (abs(cos(latitude)) * 69.172)
+        let longDelta = distance / (abs(cos(latitude)) * 110574.0)
 
         return SearchBounds(latDelta: latDelta, longDelta: longDelta)
     }
